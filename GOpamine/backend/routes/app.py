a6adc77b04561efc884ddcb7ad49fa4fa2623f6c
nguyen-1610 <duynguyenhuynh250@gmail.com>
2025-11-22 19:20:13 +0700

    chỉ demo chưa liên kết db

--- GOpamine/backend/routes/app.py
+++ GOpamine/backend/routes/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from routes.Map_Trans import map_trans
 import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
@@ -9,11 +8,6 @@ app = Flask(
 	template_folder=os.path.join(BASE_DIR, 'frontend', 'templates'),
 	static_folder=os.path.join(BASE_DIR, 'frontend', 'static')
 )
-
-app = Flask(__name__)
-
-# Đăng ký blueprint
-app.register_blueprint(map_trans)
 
 @app.route('/')
 def index():
