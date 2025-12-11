d3956b27d011c38cf1e76a4010a35d27c0aba044
Văn Hoàng Bảo <premierleague260@gmail.com>
2025-12-11 09:04:18 +0700

    Hotfix tính năng đăng nhập khách

--- GOpamine/frontend/static/js/auth.js
+++ GOpamine/frontend/static/js/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://127.0.0.1:5000';
+const API_BASE = window.location.origin;
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log(">>> AUTH JS LOADED. TARGET:", API_BASE);
